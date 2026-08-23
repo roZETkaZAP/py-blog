aa0b0459ae4891223614cd0d894ca0867746b3f7
Igor <zalugin.iv2@gmail.com>
2026-08-23 11:21:37 +0200

    minor changes

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import Group
 
 
 admin.site.register(User)
-admin.site.register(Post)
 admin.site.register(Commentary)
 admin.site.unregister(Group)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "created_time")
+    search_fields = ("title", "content")
+    list_filter = ("owner",)
--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from blog.views import IndexView, PostDetailView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("blog/<int:pk>/", PostDetailView.as_view(), name="post-detail")
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail")
 ]
 
 app_name = "blog"
